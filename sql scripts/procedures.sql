f95bb92ddb0635b88d7fafc9e929336ecfa6bbe6
lkv971 <leeroyvaillant@outlook.com>
2024-07-30 21:32:10 -0400

    Update procedures.sql

--- sql scripts/procedures.sql
+++ sql scripts/procedures.sql
@@ -1,6 +1,56 @@
 USE LogisticsDB;
 GO
 
+CREATE PROCEDURE InsertTrucks
+@Plate VARCHAR(20),
+@Brand CHAR(2),
+@TruckType VARCHAR(20),
+@TrailersType VARCHAR(20),
+@Year INT
+AS
+BEGIN
+INSERT INTO Vehicles (
+Plate,
+Brand,
+TruckType,
+TrailersType,
+Year
+)
+VALUES (
+@Plate,
+@Brand,
+@TruckType,
+@TrailersType,
+@Year
+)
+SELECT SCOPE_IDENTITY() NewVehicle
+END
+;
+GO
+
+CREATE PROCEDURE RemoveTrucks
+@TruckID INT
+AS
+BEGIN
+DELETE FROM Vehicles
+WHERE TruckID = @TruckID
+END
+;
+GO
+
+CREATE PROCEDURE UpdateTruckPlate
+@TruckID INT,
+@Plate VARCHAR(15)
+AS
+BEGIN
+UPDATE Vehicles
+SET Plate = @Plate
+WHERE TruckID = @TruckID
+END
+;
+GO
+
+
 CREATE PROCEDURE InsertCustomers
 @City VARCHAR(30),
 @State CHAR(2)
@@ -54,51 +104,3 @@ END
 ;
 GO
 
-CREATE PROCEDURE InsertTrucks
-@Plate VARCHAR(20),
-@Brand CHAR(2),
-@TruckType VARCHAR(20),
-@TrailersType VARCHAR(20),
-@Year INT
-AS
-BEGIN
-INSERT INTO Vehicles (
-Plate,
-Brand,
-TruckType,
-TrailersType,
-Year
-)
-VALUES (
-@Plate,
-@Brand,
-@TruckType,
-@TrailersType,
-@Year
-)
-SELECT SCOPE_IDENTITY() NewVehicle
-END
-;
-GO
-
-CREATE PROCEDURE RemoveTrucks
-@TruckID INT
-AS
-BEGIN
-DELETE FROM Vehicles
-WHERE TruckID = @TruckID
-END
-;
-GO
-
-CREATE PROCEDURE UpdateTruckPlate
-@TruckID INT,
-@Plate VARCHAR(15)
-AS
-BEGIN
-UPDATE Vehicles
-SET Plate = @Plate
-WHERE TruckID = @TruckID
-END
-;
-GO
