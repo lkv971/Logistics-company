173d138ec77dde2fe1918631edec9b6ff1aac292
lkv971 <leeroyvaillant@outlook.com>
2024-07-31 14:51:04 -0400

    Update triggers.sql

--- sql scripts/triggers.sql
+++ sql scripts/triggers.sql
@@ -2,6 +2,150 @@ USE LogisticsDB
 ;
 GO
 
+CREATE TRIGGER TrgInsertCost
+ON Costs
+AFTER INSERT
+AS 
+BEGIN
+INSERT INTO CostsAudit (
+TruckID,
+DriverID,
+ChangeType,
+ChangeDate,
+NewDate,
+NewKmTravelled,
+NewLitersConsumption,
+NewFuelCost,
+NewMaintenanceCost,
+NewFixedCost
+)
+SELECT
+i.TruckID,
+i.DriverID,
+'INSERT',
+GETDATE(),
+i.Date,
+i.KmTravelled,
+i.LitersConsumption,
+i.FuelCost,
+i.MaintenanceCost,
+i.FixedCost
+FROM inserted i
+;
+END
+;
+GO
+
+CREATE TRIGGER TrgDeleteCost
+ON Costs
+AFTER DELETE
+AS 
+BEGIN
+INSERT INTO CostsAudit (
+TruckID,
+DriverID,
+ChangeType,
+ChangeDate,
+OldDate,
+OldKmTravelled,
+OldLitersConsumption,
+OldFuelCost,
+OldMaintenanceCost,
+OldFixedCost
+)
+SELECT
+d.TruckID,
+d.DriverID,
+'DELETE',
+GETDATE(),
+d.Date,
+d.KmTravelled,
+d.LitersConsumption,
+d.FuelCost,
+d.MaintenanceCost,
+d.FixedCost
+FROM deleted d
+;
+END
+;
+GO
+
+CREATE Trigger TrgInsertFreight
+ON Freight
+AFTER INSERT
+AS 
+BEGIN 
+INSERT INTO FreightAudit (
+CustomerID,
+TruckID,
+ChangeType,
+ChangeDate,
+NewDate,
+NewInvoiceNumber,
+NewFreightID,
+NewCity,
+NewRevenue,
+NewWeightKg,
+NewWeightCubic,
+NewGoodsValue
+)
+SELECT
+i.CustomerID,
+i.TruckID,
+'INSERT',
+GETDATE(),
+i.Date,
+i.InvoiceNumber,
+i.FreightID,
+i.City,
+i.Revenue,
+i.WeightKg,
+i.WeightCubic,
+i.GoodsValue
+FROM inserted i
+;
+END
+;
+GO
+
+CREATE Trigger TrgDeleteFreight
+ON Freight
+AFTER DELETE
+AS 
+BEGIN 
+INSERT INTO FreightAudit (
+CustomerID,
+TruckID,
+ChangeType,
+ChangeDate,
+OldDate,
+OldInvoiceNumber,
+OldFreightID,
+OldCity,
+OldRevenue,
+OldWeightKg,
+OldWeightCubic,
+OldGoodsValue
+)
+SELECT
+d.CustomerID,
+d.TruckID,
+'DELETE',
+GETDATE(),
+d.Date,
+d.InvoiceNumber,
+d.FreightID,
+d.City,
+d.Revenue,
+d.WeightKg,
+d.WeightCubic,
+d.GoodsValue
+FROM deleted d
+;
+END
+;
+GO
+
 CREATE TRIGGER TrgInsertDriver
 ON Drivers
 AFTER INSERT
@@ -23,6 +167,32 @@ FROM inserted i
 END
 ;
 GO
+
+CREATE TRIGGER TrgUpdateDriver
+ON Drivers
+AFTER UPDATE
+AS 
+BEGIN 
+INSERT INTO DriverAudit (
+DriverID,
+ChangeType,
+ChangeDate,
+OldDriver,
+NewDriver
+)
+SELECT
+d.DriverID,
+'UPDATE',
+GETDATE(),
+d.Driver,
+i.Driver
+FROM deleted d 
+INNER JOIN inserted i
+ON d.DriverID = i.DriverID
+;
+END
+GO
+;
 
 CREATE TRIGGER TrgRemoveDriver
 ON Drivers
